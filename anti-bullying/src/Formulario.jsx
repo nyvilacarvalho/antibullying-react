3e9387eb8d26b65b4f12bec859a7eb1a133a1770
nyvila carvalho <nyvilacarvalho@gmail.com>
2025-06-30 18:04:51 -0300

    enviando arquivos

---
+++ anti-bullying/src/Formulario.jsx
@@ -0,0 +1,110 @@
+import { useState } from 'react'
+
+function Formulario() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <div className="container">
+
+      <div className="boll-1"></div>
+      <div className="boll-2"></div>
+      <div className="triangle"></div>
+
+
+      <div className="header">
+        <h1>Denuncie casos de bullying de forma anônima e segura.</h1>
+        <h2>Sua voz <span className="color">importa!</span></h2>
+      </div>
+         
+
+<div className="identification">
+    <div className="box-1">
+      <label htmlFor="nome">Nome</label><br/>
+      <input type="text" name="text"  placeholder="Nome completo"/>
+    </div>
+
+    <div className="box-2">
+      <label htmlForfor="nome">Email</label><br/>
+      <input type="email" name="email" placeholder="ava.wright@gmail.com"/>
+    </div>
+  </div>
+  
+  <div className="box-3">
+    <p>Deseja fazer a denúncia de forma anônima?</p>
+    <label htmlfor="anonimo-sim">Sim</label>
+    <input type="radio" className="custom-radio" name="anonimo"/>
+    
+    <label htmlForfor="anonimo-nao">Não</label>
+    <input type="radio" className="custom-radio" name="anonimo"/>
+  </div>
+
+<div className="infos">
+ <h3>informações sobre o Bullying</h3>
+<p>Descrição do ocorrido</p>
+<textarea rows="5"  placeholder="Descrição do fato..."></textarea>
+</div>
+              
+            <div className="infos">
+                  <h3>Tipo de bullying:</h3>
+                   <input type="radio" className="custom-radio" /> 
+                   <label for="type">Físico (agressão, empurroções, etc.)</label><br/>
+                
+                  <input type="radio" className="custom-radio" /> 
+                  <label for="type">Verbal (xingamentos, insultodos, etc.)</label><br/>
+                  
+                  <input type="radio" className="custom-radio"/> 
+                  <label for="type">Cyberbullying (mensagens ofensivas, exposição, etc.)</label><br/>
+                
+                  <input type="radio" class="custom-radio"/>
+                  <label for="type">Exclusão social (exclusão de grupos, isolamento, etc.)</label>
+                </div>
+
+      <div className="infos">
+     <h3>Informações sobre o(s) Envolvido(s)</h3>
+
+     <p>Quem praticou o bullying?</p>
+     <input type="text" name="" />
+</div>
+
+<div className="infos">
+     <p>Acontece com frequência?</p>
+     <label htmlfor="">Sim</label>
+      <input type="radio" name="select-1" className="custom-radio"/>
+      <label htmlfor="">Não</label>
+     <input type="radio" name="select-1" className="custom-radio"/>
+
+    </div>
+    
+    <div className="infos">
+     <p>Já denunciou antes?</p>
+     <label>Sim</label>
+     <input type="radio" className="custom-radio" name="select-2"/>
+
+      <label>Não</label>
+      <input type="radio" class="custom-radio" name="select-2"/>
+    </div>
+
+
+    <div className="infos">
+     <p>Local onde aconteceu?</p>
+     <input type="text" name="" />
+    </div>
+
+    <div className="infos">
+      <label for="fileUpload">Evidencias</label>
+      <input type="file" id="fileUpload" name="arquivo"/>
+    </div>
+
+    <div className="infos">
+     <button type="submit">Enviar denúncia</button>    
+</div>
+
+</div>
+    
+    </>
+  )
+}
+
+export default Formulario
